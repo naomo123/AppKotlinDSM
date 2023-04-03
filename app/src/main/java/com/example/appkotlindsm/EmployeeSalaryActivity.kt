160fa9a92db2ddc6b44e9b704bbc63cc694b904f
igles <naomoiglesias@gmail.com>
2023-04-03 17:44:16 -0600

    Finalizando funcionalidades del crud

--- app/src/main/java/com/example/appkotlindsm/EmployeeSalaryActivity.kt
+++ app/src/main/java/com/example/appkotlindsm/EmployeeSalaryActivity.kt
@@ -10,6 +10,7 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import android.widget.EditText
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appkotlindsm.modelo.Employee
@@ -30,35 +31,23 @@ class EmployeeSalaryActivity : AppCompatActivity() {
     private var selectedEmployee: Employee? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employee_salary)
-
+        recyclerView = findViewById(R.id.RVempleados)
         // Inicializar database
         database = FirebaseDatabase.getInstance().getReference("employees")
 
         // Inicializar vistas
         etNombre = findViewById(R.id.et_name)
         etSalario = findViewById(R.id.et_salary)
-        recyclerView = findViewById(R.id.RVempleados)
 
-        val agregar: FloatingActionButton = findViewById<FloatingActionButton>(R.id.calc_btn2)
-
-        // Inicializar lista de empleados y adaptador
         listemployees = mutableListOf()
-        adapterEmployee = Adapter_employee(listemployees, onItemClickListener = { employee ->
-            // Al hacer click largo en un item, cambiar a modo edición
-            selectedEmployee = employee
-            etNombre.setText(employee.nombre)
-            etSalario.setText(employee.salario.toString())
-            isEditMode = true
-        })
 
 
-        // Configurar RecyclerView
-        recyclerView.adapter = adapterEmployee
-        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Obtener lista de empleados de Firebase Database y actualizar RecyclerView
+        // Obtener lista de empleados de Firebase Database
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 // Limpiar la lista de empleados existentes
@@ -70,11 +59,25 @@ class EmployeeSalaryActivity : AppCompatActivity() {
                     employee?.let {
                         // Asignar el ID del documento al objeto empleado
                         it.id = employeeSnapshot.key ?: ""
-                        it.documentId = employeeSnapshot.key ?: ""
                         // Añadir el objeto empleado a la lista
                         listemployees.add(it)
                     }
                 }
+
+                // Inicializar lista de empleados y adaptador
+                adapterEmployee = Adapter_employee(listemployees, onItemClickListener = { employee ->
+                    // Al hacer click largo en un item, cambiar a modo edición
+                    selectedEmployee = employee
+                    etNombre.setText(employee.nombre)
+                    etSalario.setText(employee.salario.toString())
+                    isEditMode = true
+                })
+
+
+                // Configurar RecyclerView
+
+                recyclerView.adapter = adapterEmployee
+                recyclerView.layoutManager = LinearLayoutManager(this@EmployeeSalaryActivity)
 
                 // Notificar al adaptador que la lista ha cambiado
                 adapterEmployee.notifyDataSetChanged()
@@ -85,40 +88,66 @@ class EmployeeSalaryActivity : AppCompatActivity() {
                 Toast.makeText(this@EmployeeSalaryActivity, "Error al obtener lista de empleados", Toast.LENGTH_SHORT).show()
             }
         })
-        getEmployees()
-        //agregar
-        val nombre = etNombre.text.toString()
-        val salario = etSalario.text.toString().toDoubleOrNull()
+
+
+        val agregar: FloatingActionButton = findViewById<FloatingActionButton>(R.id.calc_btn2)
         agregar.setOnClickListener {
-
-            createEmployee(nombre, salario)
-            Log.d(TAG, "al menos si hace algo")
-            Log.d(TAG, etNombre.text.toString())
-            Log.d(TAG, etSalario.text.toString())  }
-    }
-
-    fun getEmployees(){
-        database.child(Employee.CHILD_NAME).addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                listemployees.clear()
-                for (employeeSnapshot in snapshot.children) {
-                    val employee = Employee.fromSnapshot(employeeSnapshot)
-                    listemployees.add(employee)
+            val nombre = etNombre.text.toString()
+            val salario = etSalario.text.toString().toDoubleOrNull()
+            if (isEditMode) {
+                // Actualizar empleado existente
+                selectedEmployee?.let { employee ->
+                    employee.nombre = nombre
+                    employee.salario = salario ?: 0.0
+                    updateEmployee(employee)
                 }
-                adapterEmployee.notifyDataSetChanged()
+            } else {
+                // Agregar nuevo empleado
+                createEmployee(nombre, salario)
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.e(TAG, "Error al leer los datos de Firebase", error.toException())
+            // Limpiar los campos de texto y desactivar el modo edición
+            etNombre.setText("")
+            etSalario.setText("")
+            isEditMode = false
+            selectedEmployee = null
+
+        }
+
+        //eliminar
+        val touchHelperCallback: ItemTouchHelper.SimpleCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return false
             }
-        })
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // Eliminar el elemento seleccionado de la base de datos
+                listemployees.get(viewHolder.adapterPosition).id?.let { database.child(it).setValue(null) }
+
+                // Eliminar el elemento de la lista y notificar al adaptador
+                listemployees.removeAt(viewHolder.adapterPosition)
+                adapterEmployee.notifyItemRemoved(viewHolder.adapterPosition)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(touchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
     fun createEmployee(nombre: String, salario: Double?) {
-        // Crear un nuevo objeto Employee con los datos proporcionados
-        val newEmployee = Employee(nombre = nombre, salario = salario ?: 0.0)
+        // Validar que se haya ingresado un nombre y un salario
+        if (nombre.isBlank() || salario == null) {
+            Toast.makeText(this, "Debe ingresar un nombre y un salario válido", Toast.LENGTH_SHORT).show()
+            return
+        }
 
+
+
+        // Crear un nuevo objeto Employee con los datos proporcionados
+        val newEmployee = Employee(nombre = nombre, salario = salario)
+        Log.d(TAG, newEmployee.toString())
         // Escribir los datos del nuevo empleado en la base de datos
-        val newEmployeeReference = database.child(Employee.CHILD_NAME).push()
+
+        val newEmployeeReference = database.push()
         newEmployeeReference.setValue(newEmployee.toMap())
 
         // Mostrar un mensaje de éxito al usuario
@@ -131,11 +160,22 @@ class EmployeeSalaryActivity : AppCompatActivity() {
         selectedEmployee = null
     }
 
+    private fun updateEmployee(employee: Employee) {
+        // Validar que se haya ingresado un nombre y un salario
+        if (employee.nombre.isBlank() || employee.salario == null) {
+            Toast.makeText(this, "Debe ingresar un nombre y un salario válido", Toast.LENGTH_SHORT).show()
+            return
+        }
 
+        // Actualizar los datos del empleado en la base de datos
+        database.child(employee.id).setValue(employee.toMap())
 
+        // Mostrar un mensaje de éxito al usuario
+        Toast.makeText(this, "Empleado actualizado correctamente", Toast.LENGTH_SHORT).show()
 
-
-
+        // Notificar al adaptador que la lista ha cambiado
+        adapterEmployee.notifyDataSetChanged()
+    }
 
 
     /** Menu creation and actions */
--- app/src/main/java/com/example/appkotlindsm/SplashActivity.kt
+++ app/src/main/java/com/example/appkotlindsm/SplashActivity.kt
@@ -14,6 +14,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 9000)
+        }, 2000)
     }
 }
